f39ed4823a7787bb3640c1f39a871d895b62d9b4
Ruan Silva <ruan.fox.2013@gmail.com>
2023-07-29 15:40:48 -0300

    adc.componentes

---
+++ src/componetes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+
+function Banner() {
+    return(
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="O banner princinpal da página do organo"/>
+        </header>
+    )
+
+}
+
+export default Banner
