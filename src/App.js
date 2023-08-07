18c7842686cbe8432c6ab7a0d1ceb6290a1cc53f
Ruan Silva <ruan.fox.2013@gmail.com>
2023-08-07 15:54:26 -0300

    style.times

--- src/App.js
+++ src/App.js
@@ -1,21 +1,75 @@
-import { useState } from 'react';
-import Banner from './componetes/Banner/Banner';
-import Formulario from './componetes/Formulario';
-
+import { useState } from "react";
+import Banner from "./componetes/Banner/Banner";
+import Formulario from "./componetes/Formulario";
+import Time from "./componetes/Time";
 
 function App() {
+  const times = [
+    {
+      nome: "Programação",
+      corPrimaria: "#57C278",
+      corSecundaria: "#D9F7E9",
+    },
 
-  const [colaboradores, setColaboradores] = useState([])
+
+    {
+      nome: "Front-End",
+      corPrimaria: "#82CFFA",
+      corSecundaria: "#E8F8FF",
+    },
+
+    {
+      nome: "Data Science",
+      corPrimaria: "#A6D157",
+      corSecundaria: "#F0F8E2",
+    },
+
+    {
+      nome: "Devops",
+      corPrimaria: "#E06B69",
+      corSecundaria: "#FDE7E8",
+    },
+
+    {
+      nome: "UX e Design",
+      corPrimaria: "#0B6EBF",
+      corSecundaria: "#FAE9F3",
+    },
+
+    {
+      nome: "Mobile",
+      corPrimaria: "#FFBA05",
+      corSecundaria: "#FFF5D9",
+    },
+
+    {
+      nome: "Inovação e Gestão",
+      corPrimaria: "#FF8A29",
+      corSecundaria: "#FFEEDF",
+    },
+
+
+
+
+  ];
+
+  const [colaboradores, setColaboradores] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador)
-  setColaboradores([...colaboradores, colaborador])
-  }
+    console.log(colaborador);
+    setColaboradores([...colaboradores, colaborador]);
+  };
 
   return (
     <div className="App">
       <Banner />
-      <Formulario aoColaboradorCadastrado = {colaborador => aoNovoColaboradorAdicionado(colaborador)} />
+      <Formulario
+        aoColaboradorCadastrado={(colaborador) =>
+          aoNovoColaboradorAdicionado(colaborador)
+        }
+      />
+      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria} />)}
+
     </div>
   );
 }
